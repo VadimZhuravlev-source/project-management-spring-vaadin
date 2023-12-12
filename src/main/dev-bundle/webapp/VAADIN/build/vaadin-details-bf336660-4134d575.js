feb4ecd7244240af7d935e04c6d119a4a21b9f06
Vadim <vaduhaj@gmail.com>
2023-12-12 14:09:21 +0400

    deleted files

--- src/main/dev-bundle/webapp/VAADIN/build/vaadin-details-bf336660-4134d575.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./indexhtml-ffb19a75.js";const t={tagName:"vaadin-details",displayName:"Details",elements:[{selector:"vaadin-details",displayName:"Root element",properties:e}]};export{t as default};
